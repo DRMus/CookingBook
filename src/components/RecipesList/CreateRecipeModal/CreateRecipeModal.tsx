561800f98f5bb612bba47f3dd2aa57bf108def23
Damir Muslimov <78694252+DRMus@users.noreply.github.com>
2023-09-20 13:24:25 +0400

    Добавление листа формы в создание рецепта

--- src/components/RecipesList/CreateRecipeModal/CreateRecipeModal.tsx
+++ src/components/RecipesList/CreateRecipeModal/CreateRecipeModal.tsx
@@ -1,27 +1,40 @@
-import { Button, Form, Input, Modal } from "antd";
+import { Button, Form, Input, Modal, Select, Space, Typography } from "antd";
 
 import "./CreateRecipeModal.scss";
+import { useState } from "react";
 
 interface Props {
   isModalOpen: boolean;
   changeModalState: (state: boolean) => void;
 }
 
+const {Text} = Typography
+
 const CreateRecipeModal = ({ isModalOpen, changeModalState }: Props) => {
   const [form] = Form.useForm();
+
+  const [ingredientsList, setIngredientsList] = useState([
+    { ingredient: "Сахар", alternate_ingredient: "Сахар, Мука" },
+  ]);
+
   const handleCancel = () => {
     changeModalState(false);
   };
   return (
     <Modal title="Создание рецепта" open={isModalOpen} onCancel={handleCancel}>
       <Form form={form} onFinish={(e) => console.log(e)}>
-        <Form.List name="some" initialValue={["1", "2"]}>
-          {(fields) => (
+        <Form.List name="some" initialValue={ingredientsList}>
+          {(fileds, { remove }) => (
             <div>
-              {fields.map((field) => (
-                <Form.Item {...field}>
-                  <Input />
-                </Form.Item>
+              {fileds.map((field, index) => (
+                <Space>
+                  <Form.Item label={index + 1} key={field.key + "ing"} name={[field.name, "ingredient"]}>
+                    <Input placeholder="Название ингредиента" />
+                  </Form.Item>
+                  <Form.Item key={field.key + "altIng"} name={[field.name, "alternate_ingredient"]}>
+                    <Input placeholder="Алтернативные ингредиенты" />
+                  </Form.Item>
+                </Space>
               ))}
             </div>
           )}
